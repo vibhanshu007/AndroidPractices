20170b4a31405afbd379cdd83b80bb1ba00f74af
vibhasnhu007 <vibhanshumail849@gmail.com>
2015-10-19 23:09:26 +0530

    Lunch Google Market..

--- src/com/example/androidpractices/MainActivity.java
+++ src/com/example/androidpractices/MainActivity.java
@@ -4,36 +4,60 @@ import com.example.androidpractices.R;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-	Button button1;
+	Button button1,button2,button3,button4;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		button1=(Button) findViewById(R.id.click_button1);
+		button1=(Button)findViewById(R.id.click_button1);
+		button2=(Button)findViewById(R.id.click_button2);
+		button3=(Button)findViewById(R.id.click_button3);
+		button4=(Button)findViewById(R.id.click_button4);
 		button1.setOnClickListener(o);
-		
+		button2.setOnClickListener(o);
+		button3.setOnClickListener(o);
+		button4.setOnClickListener(o);
+
 	}
 	OnClickListener o = new OnClickListener() {
-		
+
+		Intent intent=null , cooser=null;
 		@Override
 		public void onClick(View v) {
-			Log.d("Vibs", "Button is clicked..");
 			if(v.getId()==R.id.click_button1){
-				
+				dosomthing(button1);
+			}
+			if(v.getId()==R.id.click_button2){
+				intent=new Intent(android.content.Intent.ACTION_VIEW);
+				cooser=Intent.createChooser(intent, "Lunch Map..");
+				intent.setData(Uri.parse("geo:19,70"));
+				startActivity(cooser);
+
+			} 
+			if (v.getId()==R.id.click_button3){
+				intent=new Intent(android.content.Intent.ACTION_VIEW);
+				intent.setData(Uri.parse("https://play.google.com/store/books/details/Google_Android_Quick_Start_Guide_Android_5_0_Lolli?id=yCRZBwAAQBAJ&hl=en"));
+				cooser=Intent.createChooser(intent, "Market");
+				startActivity(cooser);
+
+			}
+			if(v.getId()==R.id.click_button4){
+
 			}
 		}
 	};
-	
+
 	public void dosomthing(View v) {
 		Intent i = new Intent(this, ActivityB.class);
 		startActivity(i);
 	}
+
 }
